4a1ca1861575fe3480c195eecf4e3870860eb4f4
Sunny Pramod Bhagwat <sunnybhagwat35@gmail.com>
2021-08-13 15:04:29 +0530

    Templates and Urls added for password reset feature

--- crm1/settings.py
+++ crm1/settings.py
@@ -133,8 +133,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img')
 #SMTP Configuration
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = ''
-EMAIL_PORT = ''
-EMAIL_USE_TLS = ''
-EMAIL_HOST_USER = 'sunnybhagwat35@gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'sunnybhagwat2000@gmail.com'
 EMAIL_HOST_PASSWORD = 'Sunny@3445'
